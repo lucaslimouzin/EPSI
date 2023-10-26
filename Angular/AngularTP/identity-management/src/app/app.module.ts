c06e3f2a7e07aaff75027b0c9c6f8a064e47ac76
lucaslimouzin <limouzin.lucas@gmail.com>
2023-10-26 17:46:45 +0200

    ajout list utilisateur

--- Angular/AngularTP/identity-management/src/app/app.module.ts
+++ Angular/AngularTP/identity-management/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/AngularTP/identity-management/src/app/ldap-list/ldap-list.component.ts
+++ Angular/AngularTP/identity-management/src/app/ldap-list/ldap-list.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { LDAP_USERS } from 'src/models/ldap-mock-data';
+import { UserLdap } from 'src/models/user-ldap';
 
 @Component({
   selector: 'app-ldap-list',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./ldap-list.component.css']
 })
 export class LdapListComponent {
+  displayedColumns: string[] = ['nomComplet', 'mail', 'employeNumero'];
+  dataSource: MatTableDataSource<UserLdap> = new MatTableDataSource<UserLdap>([]);
 
+  @ViewChild(MatPaginator, { static: true })
+  paginator!: MatPaginator | null;
+
+  ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.getUsers();
+  }
+
+  private getUsers(): void {
+    this.dataSource.data = LDAP_USERS;
+  }
 }
