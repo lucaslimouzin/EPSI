a5cb7d51afeb322afaa03d6155ae6ad49e5c5fca
lucaslimouzin <limouzin.lucas@gmail.com>
2023-10-26 15:22:34 +0200

    install TD1 angular

---
+++ Angular/AngularTP/identity-management/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LdapListComponent } from './ldap-list/ldap-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppMaterialModule } from './app-material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LdapListComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AppMaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
