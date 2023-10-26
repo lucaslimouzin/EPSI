6db689bdc5628d71ff108df75971ebe005cc9022
lucaslimouzin <limouzin.lucas@gmail.com>
2023-10-26 16:52:34 +0200

    navbar ajout

--- Angular/AngularTP/identity-management/src/app/app.module.ts
+++ Angular/AngularTP/identity-management/src/app/app.module.ts
@@ -6,17 +6,29 @@ import { AppComponent } from './app.component';
 import { LdapListComponent } from './ldap-list/ldap-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppMaterialModule } from './app-material.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     LdapListComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AppMaterialModule
+    AppMaterialModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
