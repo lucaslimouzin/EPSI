f845e467c54eb9d0ae8cd91a367d04fc63b55a74
Givros <limouzin.lucas@gmail.com>
2023-12-11 09:04:33 +0100

    python

--- Python/ProgrammationCours2/PythonTP/testBotDiscord.py
+++ Python/ProgrammationCours2/PythonTP/testBotDiscord.py
@@ -1,30 +1,25 @@
 import discord
 from discord.ext import commands
+from discord_slash import SlashCommand
 
-# Spécifiez les intents nécessaires
-intents = discord.Intents.default()
-intents.messages = True
-intents.guilds = True
-intents.reactions = True
-
-# Créez une instance de bot avec les intents spécifiés
-bot = commands.Bot(command_prefix='!', intents=intents)
+# Créer une instance du bot avec un préfixe
+bot = commands.Bot(command_prefix='!')
+slash = SlashCommand(bot, sync_commands=True)
 
 # Événement de démarrage du bot
 @bot.event
 async def on_ready():
     print(f'Bot connecté en tant que {bot.user.name}')
 
-# Commande simple
-@bot.command(name='hello')
+# Commande slash simple
+@slash.slash(name="hello", description="Salut!")
 async def hello(ctx):
     await ctx.send('Salut!')
 
-# Commande avec argument
-@bot.command(name='repeat')
-async def repeat(ctx, *args):
-    response = ' '.join(args)
-    await ctx.send(response)
+# Commande slash avec argument
+@slash.slash(name="repeat", description="Répéter un message")
+async def repeat(ctx, message: str):
+    await ctx.send(message)
 
 # Lancer le bot avec le token
-bot.run('MTE3MzYzMDA5OTE4MzExNjM2OQ.G0vkN_.s6T9cxhsoW5Kcce3uFai26O4YO2mBDD5A1iXYw', intents = intents)
+bot.run('MTE3MzYzMDA5OTE4MzExNjM2OQ.GZOfmK.YZ6o8Cu2aDVlimbByFuymb6uAwOH32uCflkfQ8')
